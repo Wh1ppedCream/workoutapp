c0817faf93216e36f9d8791ff61b4c606f4a50d0
Talha <talh768@gmail.com>
2026-08-09 17:08:10 -0400

    Harden Android emulator workflow

---
+++ test/android_device_workflow_contract_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Android device workflow isolates cold builds from test execution', () {
+    final workflow =
+        File('.github/workflows/android-device.yml').readAsStringSync();
+
+    final buildIndex = workflow.indexOf('name: Build debug APK');
+    final deviceTestIndex = workflow.indexOf(
+      'name: Run device-level core flows',
+    );
+    expect(buildIndex, greaterThanOrEqualTo(0));
+    expect(deviceTestIndex, greaterThan(buildIndex));
+    expect(workflow, contains('flutter build apk --debug'));
+    expect(workflow, contains('--timeout 15m'));
+    expect(workflow, contains('adb logcat -d -t 1000 || true'));
+  });
+}
